9fb61826b93f7cecf1e61bfc46837854f5072d41
Firhan Ramadhan <rfirhan99@gmail.com>
2022-05-01 04:44:28 +0700

    feat: create input for searching books of category

--- src/components/BookLists/index.jsx
+++ src/components/BookLists/index.jsx
@@ -1,16 +1,22 @@
+/* eslint-disable consistent-return */
 /* eslint-disable radix */
 /* eslint-disable no-nested-ternary */
 import BookItem from 'components/BookItem';
 import BookSkeleton from 'components/BookSkeleton';
 import SearchBooksInput from 'components/SearchBooksInput';
 import { useBooksByCategory } from 'features/books/queries';
-import { useState } from 'react';
-// import { useQuery } from 'react-query';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDebounce } from 'utils/hooks/useDebounce';
+import notFoundImg from 'assets/images/not-found.png';
+import img404 from 'assets/images/404.png';
 
 function BookLists() {
     const [page] = useState(1);
     const [booksResult, setBooksResult] = useState([]);
+    const [querySearch, setQuerySearch] = useState('');
+
+    const debounceSearch = useDebounce(querySearch, 500);
 
     const paramsUrl = useParams();
 
@@ -19,32 +25,83 @@ function BookLists() {
         params: {
             categoryId: paramsUrl.categoryID,
             page,
+            size: 20,
         },
         onSuccess: (res) => {
             setBooksResult(res.data);
         },
     });
 
+    const handleSearchChange = (e) => setQuerySearch(e.target.value);
+
+    useEffect(() => {
+        if (booksQuery.isSuccess) {
+            if (debounceSearch) {
+                const results = booksQuery.data.data.filter(
+                    (book) =>
+                        book.title.toLowerCase().includes(debounceSearch.toLowerCase()) ||
+                        book.authors.filter((author) => author.toLowerCase().includes(debounceSearch.toLowerCase()))
+                            .length > 0
+                );
+                return setBooksResult(results);
+            }
+
+            return setBooksResult(booksQuery.data.data);
+        }
+    }, [debounceSearch]);
+
+    const renderBooks = () => {
+        if (booksQuery.isLoading) {
+            return (
+                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                    <BookSkeleton />
+                </div>
+            );
+        }
+
+        if (booksQuery.isSuccess) {
+            if (booksResult?.length > 0 || booksQuery.data.data.length > 0) {
+                return (
+                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                        {booksResult?.map((book) => (
+                            <BookItem
+                                key={book.id}
+                                coverUrl={book.cover_url}
+                                title={book.title}
+                                authors={book.authors}
+                            />
+                        ))}
+                    </div>
+                );
+            }
+            return (
+                <div className="flex items-center justify-center flex-col">
+                    <img src={notFoundImg} alt="Not Found" className="w-96" />
+                    <p className="text-xl font-bold text-slate-800 text-center mt-4">Book Not Found</p>
+                </div>
+            );
+        }
+
+        if (booksQuery.isError) {
+            return (
+                <div className="flex items-center justify-center flex-col">
+                    <img src={img404} alt="Not Found" className="w-96" />
+                    <h3 className="text-2xl font-bold text-slate-800 text-center mt-4">
+                        {booksQuery.error.response.status}
+                    </h3>
+                    <p className="text-slate-800 text-xl text-center">Something Went Wrong</p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+
     return (
         <>
-            <SearchBooksInput />
-            <section className="my-16">
-                <div className="max-w-screen-xl mx-auto py-20 px-4">
-                    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
-                        {booksQuery.isLoading ? (
-                            <BookSkeleton />
-                        ) : booksQuery.isSuccess ? (
-                            booksResult.map((book) => (
-                                <BookItem
-                                    key={book.id}
-                                    coverUrl={book.cover_url}
-                                    title={book.title}
-                                    authors={book.authors}
-                                />
-                            ))
-                        ) : null}
-                    </div>
-                </div>
+            <SearchBooksInput onChange={handleSearchChange} />
+            <section className="my-10">
+                <div className="max-w-screen-xl mx-auto py-20 px-4">{renderBooks()}</div>
             </section>
         </>
     );
--- src/components/SearchBooksInput/index.jsx
+++ src/components/SearchBooksInput/index.jsx
@@ -1,16 +1,21 @@
-function SearchBooksInput() {
+import PropTypes from 'prop-types';
+
+function SearchBooksInput({ onChange }) {
     return (
         <div className="px-4">
             <div className="w-full max-w-xl mx-auto transform -translate-y-1/2 ">
-                <form className="w-full">
-                    <input
-                        className="w-full border-transparent shadow-md p-6 rounded-md focus:outline-none t focus:border-slate-900"
-                        placeholder="Search Books..."
-                    />
-                </form>
+                <input
+                    className="w-full border-transparent shadow-md p-6 rounded-md focus:outline-none t focus:border-slate-900"
+                    placeholder="Search Books..."
+                    onChange={onChange}
+                />
             </div>
         </div>
     );
 }
+
+SearchBooksInput.propTypes = {
+    onChange: PropTypes.func.isRequired,
+};
 
 export default SearchBooksInput;
